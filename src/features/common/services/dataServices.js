4f8c840b85843fdd7a8b91383323fd6a4bb0a652
lingar <yimprogramming@gmail.com>
2021-01-13 23:06:50 +0200

    Get and Add to the data service

--- src/features/common/services/dataServices.js
+++ src/features/common/services/dataServices.js
@@ -1,3 +1,5 @@
+const itemName = "todos";
+
 function getData(dataAction) {
 
 
@@ -13,43 +15,11 @@ export function SaveData(dataAction) {
 export function removeData() {
 
 }
+
 //todo - get, add, delete , update (mark)
 export function handleData(dataAction) {
     const action = new DataAction(dataAction);
     //todo function for localstorage and liveServer separately
-    if (action.localStorage) {//todo switch
-        if (action.actionType === "SAVE") {
-            let data;
-            localStorage.getItem(action.itemType) ? data = JSON.parse( localStorage.getItem(action.itemType)) : data = [];
-            data.push(action.item);
-            localStorage.setItem('users', JSON.stringify(data));
-
-
-        }
-        else if(action.actionType === "REMOVE"){
-            let data;
-            localStorage.getItem(action.itemType) ? data = JSON.parse( localStorage.getItem(action.itemType)) : data = [];
-            console.log("localstorage before remove", data);
-
-            let itemToRemove = data.findIndex((item)=>{
-                return  item.id === action.id
-            });
-            console.log("index ? " + itemToRemove, action.id)
-
-            if (itemToRemove>-1){
-                // itemToRemove === 0 ?  data.selectedUser = data.todos[1].id: data.selectedUser = data.todos[0].id;
-                console.log("index ? " + itemToRemove)
-                data.splice(itemToRemove, 1 );
-                console.log("localstorage after remove", data);
-
-                localStorage.setItem('users', JSON.stringify(data));
-
-            }
-            // data.push(action.item);
-            // localStorage.setItem('todos', JSON.stringify(data));
-
-        }
-    }
 }
 
 export function DataAction(action) {
@@ -61,3 +31,54 @@ export function DataAction(action) {
 
 }
 
+function localStorageDataManagement(actionType, item) {
+
+
+    switch (actionType) {
+        case (actionType === "GET"): {
+            let data = [];
+            localStorage.getItem(itemName) ? data = JSON.parse(localStorage.getItem(itemName)) : data = [];
+            return Promise.resolve(data);
+        }
+        case (actionType === "POST"): {
+            let data;
+                localStorage.getItem(itemName) ? data = JSON.parse( localStorage.getItem(itemName)) : data = [];
+                data.push(item);
+                localStorage.setItem(itemName, JSON.stringify(data));
+        }
+    }
+
+    // case (actionType.actionType === "SAVE"): {
+    //     let data;
+    //     localStorage.getItem(actionType.itemType) ? data = JSON.parse( localStorage.getItem(actionType.itemType)) : data = [];
+    //     data.push(actionType.item);
+    //     localStorage.setItem('users', JSON.stringify(data));
+    //
+    //
+    // }
+    // case(actionType.actionType === "REMOVE"):{
+    //     let data;
+    //     localStorage.getItem(actionType.itemType) ? data = JSON.parse( localStorage.getItem(action.itemType)) : data = [];
+    //     console.log("localstorage before remove", data);
+    //
+    //     let itemToRemove = data.findIndex((item)=>{
+    //         return  item.id === actionType.id
+    //     });
+    //     console.log("index ? " + itemToRemove, actionType.id)
+    //
+    //     if (itemToRemove>-1){
+    //         // itemToRemove === 0 ?  data.selectedUser = data.todos[1].id: data.selectedUser = data.todos[0].id;
+    //         console.log("index ? " + itemToRemove)
+    //         data.splice(itemToRemove, 1 );
+    //         console.log("localstorage after remove", data);
+    //
+    //         localStorage.setItem('users', JSON.stringify(data));
+    //
+    //     }
+    //     // data.push(action.item);
+    //     // localStorage.setItem('todos', JSON.stringify(data));
+    //
+    // }
+
+
+}
