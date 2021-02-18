6bd85ef5c59534950fbdf25aebd8efc6ab12de0d
lingar <yimprogramming@gmail.com>
2021-02-18 13:29:04 +0200

    saving state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useReducer, useState,useLayoutEffect} from 'react';
+import React, {useEffect, useReducer, useState, useLayoutEffect} from 'react';
 import './App.css';
 import {Todos} from './features/todos/Todos';
 import {manageStore} from "./features/common/services/todos-store";
@@ -17,45 +17,64 @@ function App() {
     }
     //Instantiate the todos-store, with data, and with dispatching action (function that will happen in each dispatching of the action and will be manipulate the data
     const [todosStore, dispatchTodoAction] = useReducer(manageStore, initialTodos);
-    const [serverConnection, setServerConnection ] = useState(false);
-    useEffect(()=>{
+    const [serverConnection, setServerConnection] = useState(false);
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
         console.log("getting the data from the localstorage...")
-        localStorageDataManagementWithPromises('GET', null).then(data=>{
-            console.log("Data =  " , data);
+        localStorageDataManagementWithPromises('GET', null).then(data => {
+            console.log("Data =  ", data);
             dispatchTodoAction({
                 type: 'GET',
                 todos: data
             });
-        }).catch(()=>{
+        }).catch(() => {
             console.log("Error with getting the data");
         });
-    },[]);
+    }, []);
 
-    useLayoutEffect(()=>{
-        if (serverConnection){
-            axios.get(BASE_URL+"/test-server").then(()=>console.log("server up")).catch(()=>{
-                console.error("server down");
+    useLayoutEffect(() => {//layout because it affects the loading
+        if (serverConnection) {
+            setLoading(true);
+
+            axios.get(BASE_URL + "/test-server").then(() => console.log("server up")).catch(() => {
+                alert("server down");
                 setServerConnection(false);
+                setLoading(false);
+
             });
         }
-    })
+    }, [serverConnection])
 
     return (
         <div className="">
             <p>with the help of God</p>
-            <p>Done by Izhar Mashkif (lingar) , yimprogramming@gmail.com. <b>I am available for new projects</b> :) !</p>
-            <input type= "checkbox"
-            onChange={()=>setServerConnection(!serverConnection)}
-            checked={serverConnection}/>
+            <p>Done by Izhar Mashkif (lingar) , yimprogramming@gmail.com. <b>I am available for new projects</b> :) !
+            </p>
+            {!loading? <div>
+                <h2>Connected to {serverConnection ? "Server" : "localstorage"}</h2>
+                <h3>Switch to {!serverConnection ? "Server" : "localstorage"} connections</h3>
+                <input type="checkbox"
+                       onChange={() => setServerConnection(!serverConnection)}
+                       checked={serverConnection}/>
+            </div> : "Trying to connect" }
+
             <ul>
-                <li>Done with useReducer from react-hooks, a tool for easily manage stores. </li>
+                <li>Done with useReducer from react-hooks, a tool for easily manage stores.</li>
                 <li>It's demonstrate working with promises, and then update the store.
-                    You can change the delay time in<pre> features/common/services/data-services.js </pre>. </li>
-                <li style = {{color:"red"}}>For see error you can add description with "error" text and see what's happen</li>
+                    You can change the delay time in22
+                    <pre> features/common/services/data-services.js </pre>
+                    .
+                </li>
+                <li style={{color: "red"}}>For see error you can add description with "error" text and see what's
+                    happen
+                </li>
 
             </ul>
             {/*The todos component with todo's store to manage the data from one place*/}
             <Todos todosStore={todosStore} todosAction={dispatchTodoAction}/>
+            {loading && <div className="loader"></div>}
+            <p>l22 = {loading + ""}</p>
+
         </div>
     );
 }
